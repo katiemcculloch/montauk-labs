da22d8f6c27960a328eb82537e9bc9c22107a1df
Katie McCulloch <katie@dolthub.com>
2022-01-20 11:30:17 -0800

    Remove unused file, style organization page slightly, add blank Header

---
+++ components/Header/index.tsx
@@ -0,0 +1,7 @@
+import css from "./index.module.css";
+
+const Header = () => {
+  return <header className={css.header}/>;
+};
+
+export default Header;
--- pageComponents/OrganizationPage/index.tsx
+++ pageComponents/OrganizationPage/index.tsx
@@ -2,9 +2,10 @@ import moment from "moment";
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
 import React from "react";
+import { BiArrowBack } from "react-icons/bi";
 import { Scatter, ScatterChart, XAxis, YAxis } from "recharts";
 import { PatentWithExpDate } from "../../types";
-import css from "./index.module.css"
+import css from "./index.module.css";
 
 const Organization: NextPage<{ patents: PatentWithExpDate[] }> = ({
   patents,
@@ -29,9 +30,52 @@ const Organization: NextPage<{ patents: PatentWithExpDate[] }> = ({
     </ScatterChart>
   );
 
+  const onClickBack = () => {
+    router.push("/organizations");
+  };
+
   return (
     <div className={css.container}>
+      <div className={css.back} onClick={onClickBack}>
+        <BiArrowBack className={css.backArrow} />
+        Back to search
+      </div>
       <h3>Organization: {org}</h3>
+      <h5> Total patents: {patents.length}</h5>
+      <div className={css.patentList}>
+        {patents.map((p) => {
+          const exp = moment(p.expiration_date);
+          const pat = moment(p.patent_date);
+          return (
+            <div className={css.patentInfo}>
+              {p.applications.length > 0 && (
+                <div>Patent ID: {p.applications[0].app_id}</div>
+              )}
+              <div>Patent date: {p.patent_date}</div>
+              <div>Patent type: {p.patent_type}</div>
+
+              <div>CPCS ID(s): {p.cpcs.map((c) => c.cpc_section_id)}</div>
+
+              {p.applications.length > 0 && (
+                <div>Application date: {p.applications[0].app_date}</div>
+              )}
+              <div>
+                Expiration date: {exp.year()}-
+                {exp.month().toLocaleString("en-US", {
+                  minimumIntegerDigits: 2,
+                  useGrouping: false,
+                })}
+                -
+                {exp.day().toLocaleString("en-US", {
+                  minimumIntegerDigits: 2,
+                  useGrouping: false,
+                })}
+              </div>
+              <div>Expires {exp.fromNow()}.</div>
+            </div>
+          );
+        })}
+      </div>
       {renderLineChart}
     </div>
   );
